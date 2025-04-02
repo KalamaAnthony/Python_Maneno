4135feba416fc79c5d0d4e421925b7f3329eb752
Anthony <Kalama.Anthony@equitybank.co.ke>
2025-04-02 14:40:57 +0300

    initial commit

---
+++ EQC.py
@@ -0,0 +1,10 @@
+name= input("What is your name? ")
+print("Hello "+ name)
+X = int(input ("Enter the value for x"))
+y= int(input("Enter the value for y"))
+print("You have entered x as {} and y as {}".format(X,y))
+z=int(X*y)
+print("The product z is therefore {} ".format(z))
+
+Name = "Testing"
+print(len(Name))
---
+++ user_input.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+age = input("How old are you?")
+location=input("Where do you live?")
+print("Hello " + name + " you are " + age + "years old and you live in " + location)
+
+x= {}
+x[2]=10
+x[1]=[10,30,40]
+print(x[1][2])
+
+a= 10
+b = 3
+print(a%b)
