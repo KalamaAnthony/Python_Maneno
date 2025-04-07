ac1ec045cf095d07b5b9bcd751f16da05855b7a7
Kalama Baraka <tonnykalama9@gmail.com>
2025-04-07 21:59:34 +0300

    conditional

---
+++ conditional.py
@@ -0,0 +1,8 @@
+a= int(input ("How old are you?"))
+if a<18 :
+    print("You're below 18 years of age")
+
+elif a >18:
+    print("You're above 18 years and  eligible ")
+else:
+    print("You must be over 18 years in order to be eligible")        
